b04f5587273f8ef6abc9dad5e14dd7e3c3cfee14
Chisha14 <chisha_7@hotmail.com>
2017-07-27 11:51:46 +0200

    Update configure.sh

--- Storm/configure.sh
+++ Storm/configure.sh
@@ -15,6 +15,7 @@ if [ -n "$2" ]; then
    cat >> /usr/local/storm/apache-storm-0.9.3/conf/storm.yaml <<EOF
 nimbus.host: "$2"
 EOF
+fi
 
 cat >> /usr/local/storm/apache-storm-0.9.3/conf/storm.yaml <<EOF
 storm.zookeeper.port: 2181
