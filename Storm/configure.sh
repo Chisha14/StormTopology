db9e3ec82388942ca25b673d7da9ef200b016ad1
Chisha14 <chisha_7@hotmail.com>
2017-07-27 11:26:58 +0200

    Update configure.sh

--- Storm/configure.sh
+++ Storm/configure.sh
@@ -15,12 +15,5 @@ if [ -n "$2" ]; then
    cat >> /usr/local/storm/apache-storm-0.9.3/conf/storm.yaml <<EOF
 nimbus.host: "$2"
 EOF
-fi   
-cat >> /usr/local/storm/apache-storm-0.9.3/conf/storm.yaml <<EOF
-storm.zookeeper.port: 2181
-EOF
-#cat >> /usr/local/storm/apache-storm-0.9.3/conf/storm.yaml <<EOF
-#java.library.path: "/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.131-3.b12.el7_3.x86_64/jre/"
-#EOF
 
 cat /usr/local/storm/apache-storm-0.9.3/conf/storm.yaml
